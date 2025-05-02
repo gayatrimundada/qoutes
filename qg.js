f47fd11a80f7bbde8ea15eedbae4cbca9438d088
Gayatri Mundada <gayatrimundada111@gmail.com>
2025-05-02 16:10:20 +0530

    Quotes generator

---
+++ qg.js
@@ -0,0 +1,20 @@
+let btn=document.querySelector('button')
+console.log(btn)
+let op=document.getElementById('q')
+let quotes=[
+    "You’re braver than you believe, and stronger than you seem, and smarter than you think",
+    "Keep your face to the sunshine and you cannot see a shadow.",
+    "The only time you fail is when you fall down and stay down.",
+    "Positive anything is better than negative nothing.",
+    "Happiness is the only thing that multiplies when you share it.",
+    "The happiness of your life depends upon the quality of your thoughts.",
+    "The way I see it, if you want the rainbow, you gotta put up with the rain."
+]
+btn.addEventListener('click',() =>{
+// for(let i=1;i<=quotes.length;i++){
+    var randomquotes=quotes[Math.floor(Math.random()*quotes.length) ]
+    op.innerHTML=randomquotes
+// }
+// document.getElementById('s').textContent=quotes
+}
+)
